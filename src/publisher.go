8c5e7c06f532b8101b63544170ac41952435e477
Jake Thomas <jthomas@6river.com>
2022-02-02 17:46:18 -0500

    use wait groups

--- src/publisher.go
+++ src/publisher.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	"cloud.google.com/go/pubsub"
 	"golang.org/x/net/context"
@@ -22,18 +23,21 @@ func publishEvent(ctx context.Context, topic *pubsub.Topic, event Event) {
 }
 
 func publishEvents(ctx context.Context, topic *pubsub.Topic, events []Event) {
-	// var wg sync.WaitGroup
+	var wg sync.WaitGroup
 	for _, event := range events {
 		payload, _ := json.Marshal(event)
 		msg := &pubsub.Message{
 			Data: payload,
 		}
 		result := topic.Publish(ctx, msg)
-
-		id, err := result.Get(ctx)
-		if err != nil {
-			fmt.Printf("FIXME! something bad happened %v\n", err)
-		}
-		fmt.Printf("message %v sent to pubsub\n", id)
+		wg.Add(1)
+		go func(res *pubsub.PublishResult) {
+			defer wg.Done()
+			_, err := res.Get(ctx)
+			if err != nil {
+				fmt.Printf("FIXME! something bad happened %v\n", err)
+			}
+		}(result)
 	}
+	wg.Wait()
 }
