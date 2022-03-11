96ea0e2bc8747b7dadfd5efc544a112cce4cab72
Jake Thomas <jthomas@6river.com>
2022-03-11 01:57:34 -0500

    Start thinking about envelope and metadata - it will help eliminate instances of map[string]interface

--- pkg/event/selfDescribing.go
+++ pkg/event/selfDescribing.go
@@ -1,5 +1,27 @@
 package event
 
+import (
+	"time"
+
+	"github.com/silverton-io/honeypot/pkg/validator"
+)
+
+type HoneypotEnvelopeMetadata struct {
+	EventProtocol     string                    `json:"eventProtocol"`
+	EventVendor       string                    `json:"eventVendor"`
+	EventId           string                    `json:"eventId"`
+	EventVersion      string                    `json:"eventVersion"`
+	SchemaFormat      string                    `json:"schemaFormat"`
+	IsValid           bool                      `json:"isValid"`
+	ValidationError   validator.ValidationError `json:"validationError"`
+	HoneypotTimestamp time.Time                 `json:"honeypotTimestamp"`
+}
+
+type HoneypotEnvelope struct {
+	HoneypotEnvelopeMetadata `json:"hMetadata"`
+	HPayload                 map[string]interface{} `json:"hPayload"`
+}
+
 type SelfDescribingEnvelope struct {
 	Contexts []SelfDescribingContext `json:"contexts"`
 	Payload  SelfDescribingPayload   `json:"payload"`
---
+++ pkg/validator/jsonschema_test.go
@@ -0,0 +1,9 @@
+package validator
+
+import "testing"
+
+func TestJsonschemaValidatePayloadInvalidSchema(t *testing.T) {}
+
+func TestJsonschemaValidatePayloadValidPayload(t *testing.T) {}
+
+func TestJsonschemaValidatePayloadInvalidPayload(t *testing.T) {}
