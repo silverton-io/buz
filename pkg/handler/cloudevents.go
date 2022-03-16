922fb0b6ee51bccb2f4d699541b9b6e87c70d15e
Jake Thomas <jthomas@6river.com>
2022-03-15 23:10:26 -0400

    Get cloudevents handler running again

--- pkg/handler/cloudevents.go
+++ pkg/handler/cloudevents.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"io/ioutil"
 	"time"
@@ -15,15 +16,15 @@ import (
 	"github.com/silverton-io/honeypot/pkg/response"
 	"github.com/silverton-io/honeypot/pkg/sink"
 	"github.com/silverton-io/honeypot/pkg/tele"
-	"github.com/silverton-io/honeypot/pkg/util"
+	"github.com/silverton-io/honeypot/pkg/validator"
 	"github.com/tidwall/gjson"
 )
 
 func CloudeventsPostHandler(conf *config.Cloudevents, meta *tele.Meta, cache *cache.SchemaCache, sink sink.Sink) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		var events []event.Envelope
+		var envelopes []event.Envelope
 		if c.ContentType() == "application/cloudevents+json" { // Only accept request if content type is set appropriately
-			// ctx := context.Background()
+			ctx := context.Background()
 			reqBody, _ := ioutil.ReadAll(c.Request.Body)
 			result := gjson.ParseBytes(reqBody)
 			cloudevent := cloudevents.BuildEvent(c, result.Value().(map[string]interface{}))
@@ -33,10 +34,9 @@ func CloudeventsPostHandler(conf *config.Cloudevents, meta *tele.Meta, cache *ca
 				Tstamp:        time.Now(),
 				Payload:       cloudevent,
 			}
-			events = append(events, envelope)
-			util.Pprint(events)
-			// validEvents, invalidEvents := bifurcateEvents(cloudevents, cache)
-			// sink.BatchPublishValidAndInvalid(ctx, protocol.CLOUDEVENTS, validEvents, invalidEvents, meta)
+			envelopes = append(envelopes, envelope)
+			validEvents, invalidEvents := validator.BifurcateAndAnnotate(envelopes, cache)
+			sink.BatchPublishValidAndInvalid(ctx, protocol.CLOUDEVENTS, validEvents, invalidEvents, meta)
 			c.JSON(200, response.Ok)
 		} else {
 			c.JSON(400, response.InvalidContentType)
@@ -48,8 +48,8 @@ func CloudeventsPostHandler(conf *config.Cloudevents, meta *tele.Meta, cache *ca
 
 func CloudeventsBatchPostHandler(conf *config.Cloudevents, meta *tele.Meta, cache *cache.SchemaCache, sink sink.Sink) gin.HandlerFunc {
 	fn := func(c *gin.Context) {
-		// ctx := context.Background()
-		// var events []cloudevents.CloudEvent
+		ctx := context.Background()
+		var envelopes []event.Envelope
 		if c.ContentType() == "application/cloudevents-batch+json" {
 			reqBody, _ := ioutil.ReadAll(c.Request.Body)
 			var rawEvents []interface{}
@@ -59,19 +59,26 @@ func CloudeventsBatchPostHandler(conf *config.Cloudevents, meta *tele.Meta, cach
 				c.JSON(400, response.BadRequest)
 				return
 			}
-			// for _, rawEvent := range rawEvents {
-			// 	if err != nil {
-			// 		log.Error().Stack().Err(err).Msg("error when marshaling event")
-			// 		c.JSON(400, response.BadRequest)
-			// 		return
-			// 	} else {
-			// 		event := gjson.ParseBytes(marshaledEvent)
-			// 		cloudevent := buildCloudevent(event)
-			// 		cloudevents = append(cloudevents, cloudevent)
-			// 	}
-			// }
-			// validEvents, invalidEvents := bifurcateEvents(cloudevents, cache)
-			// sink.BatchPublishValidAndInvalid(ctx, protocol.CLOUDEVENTS, validEvents, invalidEvents, meta)
+			for _, rawEvent := range rawEvents {
+				marshaledEvent, err := json.Marshal(rawEvent)
+				if err != nil {
+					log.Error().Stack().Err(err).Msg("error when marshaling event")
+					c.JSON(400, response.BadRequest)
+					return
+				} else {
+					e := gjson.ParseBytes(marshaledEvent)
+					cloudevent := cloudevents.BuildEvent(c, e.Value().(map[string]interface{}))
+					envelope := event.Envelope{
+						EventProtocol: protocol.CLOUDEVENTS,
+						EventSchema:   &cloudevent.DataSchema,
+						Tstamp:        time.Now(),
+						Payload:       cloudevent,
+					}
+					envelopes = append(envelopes, envelope)
+				}
+			}
+			validEvents, invalidEvents := validator.BifurcateAndAnnotate(envelopes, cache)
+			sink.BatchPublishValidAndInvalid(ctx, protocol.CLOUDEVENTS, validEvents, invalidEvents, meta)
 			c.JSON(200, response.Ok)
 		} else {
 			c.JSON(400, response.InvalidContentType)
