a97701dd5c8178b703918ecfb62c2b384d15b8a1
jakthom <aerialfly@gmail.com>
2022-03-16 22:20:11 -0400

    checkpoint

--- pkg/handler/relay.go
+++ pkg/handler/relay.go
@@ -6,9 +6,15 @@ import (
 	"io/ioutil"
 
 	"github.com/gin-gonic/gin"
+	"github.com/rs/zerolog/log"
+	"github.com/silverton-io/honeypot/pkg/cloudevents"
 	"github.com/silverton-io/honeypot/pkg/event"
+	"github.com/silverton-io/honeypot/pkg/generic"
 	"github.com/silverton-io/honeypot/pkg/protocol"
+	"github.com/silverton-io/honeypot/pkg/snowplow"
+	"github.com/silverton-io/honeypot/pkg/util"
 	"github.com/silverton-io/honeypot/pkg/validator"
+	"github.com/tidwall/gjson"
 )
 
 func RelayHandler(p EventHandlerParams) gin.HandlerFunc {
@@ -16,7 +22,35 @@ func RelayHandler(p EventHandlerParams) gin.HandlerFunc {
 		var envelopes []event.Envelope
 		ctx := context.Background()
 		reqBody, _ := ioutil.ReadAll(c.Request.Body)
-		json.Unmarshal(reqBody, &envelopes)
+		relayedEvents := gjson.ParseBytes(reqBody)
+		for _, relayedEvent := range relayedEvents.Array() {
+			eventProtocol := relayedEvent.Get("eventProtocol").String()
+			eventPayload := relayedEvent.Get("payload").Raw
+			var envelope event.Envelope
+			envelopeErr := json.Unmarshal([]byte(relayedEvent.Raw), &envelope)
+			if envelopeErr != nil {
+				log.Error().Stack().Err(envelopeErr).Msg("could not unmarshal relayed event to envelope")
+			}
+			switch eventProtocol {
+			case protocol.SNOWPLOW:
+				payload := snowplow.SnowplowEvent{}
+				json.Unmarshal([]byte(eventPayload), &payload)
+				envelope.Payload = payload
+			case protocol.CLOUDEVENTS:
+				payload := cloudevents.CloudEvent{}
+				json.Unmarshal([]byte(eventPayload), &payload)
+				envelope.Payload = payload
+			case protocol.GENERIC:
+				payload := generic.GenericEvent{}
+				json.Unmarshal([]byte(eventPayload), &payload)
+				envelope.Payload = payload
+			default:
+				payload := snowplow.SnowplowEvent{}
+				json.Unmarshal([]byte(eventPayload), &payload)
+				envelope.Payload = payload
+			}
+			util.Pprint(envelope)
+		}
 		validEnvelopes, invalidEnvelopes := validator.Bifurcate(envelopes)
 		p.Sink.BatchPublishValidAndInvalid(ctx, protocol.RELAY, validEnvelopes, invalidEnvelopes, p.Meta)
 	}
