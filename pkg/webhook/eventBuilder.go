bb80f4c1305a3db399db6c0e4db80c5ee85e71c3
jakthom <aerialfly@gmail.com>
2022-03-29 08:42:10 -0400

    little fix to webhook id

--- pkg/webhook/eventBuilder.go
+++ pkg/webhook/eventBuilder.go
@@ -9,7 +9,7 @@ const DEFAULT_WEBHOOK_ID string = "f324467b-1213-46a1-b5c8-f173aa5d82e4"
 
 func BuildEvent(c *gin.Context, payload gjson.Result) (WebhookEvent, error) {
 	webhookId := c.Param(WEBHOOK_ID_PARAM)
-	if webhookId == "" {
+	if webhookId == "" || webhookId == "/" {
 		webhookId = DEFAULT_WEBHOOK_ID
 	} else {
 		webhookId = webhookId[1:]
