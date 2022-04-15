ffa921a3c2cd585a72e42d28e372b06e8a37c6a8
jakthom <aerialfly@gmail.com>
2022-04-14 22:24:11 -0400

    clean up setup for now

--- examples/devel/setup.sh
+++ examples/devel/setup.sh
@@ -3,6 +3,3 @@ for run in {1..5}; do printf "🍯" && sleep 1; done
 
 echo "\nSetting up clickhouse...\n"
 docker exec clickhouse sh -c "clickhouse-client -u honeypot --password honeypot -q \"create database honeypot;\""
-
-echo "\nSetting up materialize...\n"
-docker exec materialize sh -c "psql -"
