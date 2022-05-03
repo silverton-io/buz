4ca3455f3eb157b7cbcdafa6c4c04db4e3e4e898
jakthom <aerialfly@gmail.com>
2022-05-02 23:19:50 -0400

    avoid reserved words as defaults

--- examples/devel/setup.sh
+++ examples/devel/setup.sh
@@ -1,7 +1,7 @@
 REGISTRY_HOST=localhost
 REGISTRY_DB=honeypot
 REGISTRY_SCHEMA=honeypot
-REGISTRY_TABLE=schemas
+REGISTRY_TABLE=registry
 H_USER=honeypot
 H_PASS=honeypot
 # Seed files
@@ -10,6 +10,43 @@ MYSQL_SEED_FILE=mysqlSeed.sql
 CLICKHOUSE_SEED_FILE=clickhouseSeed.sql
 MATERIALIZE_SEED_FILE=materializeSeed.sql
 
+
+function cleanSeedFiles {
+    echo "Cleaning seed files...";
+    rm $PG_SEED_FILE || true;
+    rm $MYSQL_SEED_FILE || true;
+    rm $CLICKHOUSE_SEED_FILE || true;
+}
+
+function seedDatabaseBackends {
+    echo "\nSeeding database schema cache backends...\n"
+    cleanSeedFiles;
+    echo "delete from $REGISTRY_TABLE;" >> $PG_SEED_FILE;
+    echo "delete from $REGISTRY_TABLE;" >> $MATERIALIZE_SEED_FILE;
+    echo "delete from $REGISTRY_SCHEMA.$REGISTRY_TABLE;" >> $MYSQL_SEED_FILE;
+    echo "alter table $REGISTRY_SCHEMA.$REGISTRY_TABLE delete where 1=1;" >> $CLICKHOUSE_SEED_FILE;
+    find schemas -type f | while read fname; do
+        SCHEMA=$(echo $fname | sed 's/schemas\///')
+        CONTENTS=$(jq -c . $fname)
+        # Postgres
+        PG_CMD="insert into $REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA','$CONTENTS');"
+        echo $PG_CMD >> $PG_SEED_FILE;
+        # Materialize
+        echo $PG_CMD >> $MATERIALIZE_SEED_FILE;
+        # Mysql
+        MYSQL_CMD="insert into $REGISTRY_SCHEMA.$REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA','$CONTENTS');"
+        echo $MYSQL_CMD >> $MYSQL_SEED_FILE;
+        # Clickhouse
+        CLICKHOUSE_CMD="insert into $REGISTRY_SCHEMA.$REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA', '$CONTENTS')"
+        echo $CLICKHOUSE_CMD >> $CLICKHOUSE_SEED_FILE;
+    done
+
+    psql -h $REGISTRY_HOST -p 5432 -U $H_USER -f $PG_SEED_FILE;
+    psql -h $REGISTRY_HOST -p 6875 -U materialize -f $PG_SEED_FILE;
+    export MYSQL_PWD=honeypot; mysql -h 127.0.0.1 -u$H_USER < $MYSQL_SEED_FILE;
+    clickhouse client -h $REGISTRY_HOST  --port 9000 -u $H_USER --password $H_PASS --queries-file $CLICKHOUSE_SEED_FILE
+    cleanSeedFiles;
+}
 
 echo "\nHoneypotting...\n"
 for run in {1..5}; do printf "🍯" && sleep 1; done
@@ -27,28 +64,5 @@ rpk topic \
     create hpt-valid \
     --brokers 127.0.0.1:9092;
 
-echo "\nSeeding database schema cache backends...\n"
-echo "delete from $REGISTRY_SCHEMA.$REGISTRY_TABLE;" >> $PG_SEED_FILE;
-echo "delete from $REGISTRY_TABLE;" >> $MATERIALIZE_SEED_FILE;
-echo "delete from $REGISTRY_SCHEMA.$REGISTRY_TABLE;" >> $MYSQL_SEED_FILE;
-echo "alter table $REGISTRY_SCHEMA.$REGISTRY_TABLE delete where 1=1;" >> $CLICKHOUSE_SEED_FILE;
-find schemas -type f | while read fname; do
-    SCHEMA=$(echo $fname | sed 's/schemas\///')
-    CONTENTS=$(jq -c . $fname)
-    # Postgres
-    PG_CMD="insert into $REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA','$CONTENTS');"
-    echo $PG_CMD >> $PG_SEED_FILE;
-    # Materialize
-    echo $PG_CMD >> $MATERIALIZE_SEED_FILE;
-    # Mysql
-    MYSQL_CMD="insert into $REGISTRY_SCHEMA.$REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA','$CONTENTS');"
-    echo $MYSQL_CMD >> $MYSQL_SEED_FILE;
-    # Clickhouse
-    CLICKHOUSE_CMD="insert into $REGISTRY_SCHEMA.$REGISTRY_TABLE (created_at, updated_at, name, contents) values (now(), now(), '$SCHEMA', '$CONTENTS')"
-    echo $CLICKHOUSE_CMD >> $CLICKHOUSE_SEED_FILE;
-done
 
-psql -h $REGISTRY_HOST -p 5432 -U $H_USER -f $PG_SEED_FILE;
-psql -h $REGISTRY_HOST -p 6875 -U $H_USER -f $PG_SEED_FILE;
-export MYSQL_PWD=honeypot; mysql -h $REGISTRY_HOST -u$H_USER < $MYSQL_SEED_FILE;
-clickhouse client -h $REGISTRY_HOST  --port 9000 -u $H_USER --password $H_PASS --queries-file $CLICKHOUSE_SEED_FILE
+seedDatabaseBackends;
