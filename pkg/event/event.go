4dca633605f8d559bd4a666629408b8993c1aebe
jakthom <aerialfly@gmail.com>
2022-06-12 09:43:06 -0400

    Slim down event interface

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -6,8 +6,6 @@ type Event interface {
 	SchemaName() *string
 	Protocol() string
 	PayloadAsByte() ([]byte, error)
-	AsByte() ([]byte, error)
-	AsMap() (map[string]interface{}, error)
 	Value() (driver.Value, error)
 	Scan(input interface{}) error
 }
