2e2db6fb31af36f5b76bc834d668fbac822ad3e5
jakthom <aerialfly@gmail.com>
2022-06-12 14:38:40 -0400

    Scrap protocol from event interface

--- pkg/event/event.go
+++ pkg/event/event.go
@@ -4,7 +4,6 @@ import "database/sql/driver"
 
 type Event interface {
 	SchemaName() *string
-	Protocol() string
 	PayloadAsByte() ([]byte, error)
 	Value() (driver.Value, error)
 	Scan(input interface{}) error
