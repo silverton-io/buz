489fb21ed2736d05054087a215010e44c4e7ce74
jakthom <aerialfly@gmail.com>
2022-06-12 21:16:49 -0400

    additional docusaurus snowplow defaults

--- website/plugins/snowplow/index.js
+++ website/plugins/snowplow/index.js
@@ -29,7 +29,10 @@ module.exports = function (context) {
             ;(function(p,l,o,w,i,n,g){if(!p[i]){p.GlobalSnowplowNamespace=p.GlobalSnowplowNamespace||[]; p.GlobalSnowplowNamespace.push(i);p[i]=function(){(p[i].q=p[i].q||[]).push(arguments) };p[i].q=p[i].q||[];n=l.createElement(o);g=l.getElementsByTagName(o)[0];n.async=1; n.src=w;g.parentNode.insertBefore(n,g)}}(window,document,"script","https://cdn.jsdelivr.net/npm/@snowplow/javascript-tracker@3.4.0/dist/sp.js","snowplow"));
             snowplow('newTracker', 'sp', '${collector}', {
                 appId: '${appId}',
-                withCredentials: ${withCredentials}
+                platform: 'web',
+                withCredentials: ${withCredentials},
+                cookieSameSite: 'Lax',
+                discoverRootDomain: true
             });
             `
           },
