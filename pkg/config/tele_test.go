46fa151bdbc51c35658b3eedec87628e80a3516c
taborwells <110945928+taborwells@users.noreply.github.com>
2022-08-10 09:15:53 -0400

    Update .go files with license header (#351)
    * Update .go files with license header
    
    * AGPLv3 not GPLv3. Oops.
    
    Co-authored-by: Tabor Wells <tabor@emtech.com>

--- pkg/config/tele_test.go
+++ pkg/config/tele_test.go
@@ -1,1 +1,5 @@
+// Copyright (c) 2022 Silverton Data, Inc.
+// You may use, distribute, and modify this code under the terms of the AGPLv3 license, a copy of
+// which may be found at https://github.com/silverton-io/honeypot/blob/main/LICENSE
+
 package config
