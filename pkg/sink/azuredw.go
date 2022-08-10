84b8e844f1c0f700a2a8de2dbb9b38ef4392fbfe
Tabor Wells <tabor@emtech.com>
2022-08-09 20:17:55 -0400

    Update .go files with license header

--- pkg/sink/azuredw.go
+++ pkg/sink/azuredw.go
@@ -1,3 +1,7 @@
+// Copyright (c) 2022 Silverton Data, Inc.
+// You may use, distribute, and modify this code under the terms of the GPLv3 license, a copy of
+// which may be found at https://github.com/silverton-io/honeypot/blob/main/LICENSE
+
 package sink
 
 // FIXME! Implement azure dw sink
