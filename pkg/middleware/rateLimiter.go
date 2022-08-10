0f7673d49310d08848e98886a405d6ea18718dc0
Tabor Wells <tabor@emtech.com>
2022-08-09 20:31:04 -0400

    AGPLv3 not GPLv3. Oops.

--- pkg/middleware/rateLimiter.go
+++ pkg/middleware/rateLimiter.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Silverton Data, Inc.
-// You may use, distribute, and modify this code under the terms of the GPLv3 license, a copy of
+// You may use, distribute, and modify this code under the terms of the AGPLv3 license, a copy of
 // which may be found at https://github.com/silverton-io/honeypot/blob/main/LICENSE
 
 package middleware
