bc0c3db2fd724d4345cfb4f9e8c367dce9352e1d
Jake Thomas <aerialfly@gmail.com>
2022-08-21 02:13:39 -0400

    fix-releaser (#387)
    * Fixes releaser
    * Fixes nil pointer

---
+++ cmd/buz/main.go
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Silverton Data, Inc.
+// You may use, distribute, and modify this code under the terms of the AGPLv3 license, a copy of
+// which may be found at https://github.com/silverton-io/buz/blob/main/LICENSE
+
+package main
+
+func main() {
+	app := App{}
+	app.Initialize()
+	app.Run()
+}
