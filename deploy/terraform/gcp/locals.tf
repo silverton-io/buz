144e156a2926e83c875e5bec65d8ca747f23cbc2
jakthom <aerialfly@gmail.com>
2022-09-05 14:55:02 -0400

    Rip out locals comments so alignment is nice

--- deploy/terraform/gcp/locals.tf
+++ deploy/terraform/gcp/locals.tf
@@ -6,21 +6,17 @@ locals {
     "run.googleapis.com",
     "secretmanager.googleapis.com"
   ]
-  domain_parts    = split(".", var.buz_domain)
-  cookie_domain   = join(".", slice(local.domain_parts, 1, length(local.domain_parts))) # Assumes Buz is running on a subdomain and the cookie should be on root
-  system_env_base = "${var.system}-${var.env}-"
-  # Image
+  domain_parts                 = split(".", var.buz_domain)
+  cookie_domain                = join(".", slice(local.domain_parts, 1, length(local.domain_parts))) # Assumes Buz is running on a subdomain and the cookie should be on root
+  system_env_base              = "${var.system}-${var.env}-"
   artifact_repository          = "${local.system_env_base}repository"
   artifact_registry_location   = "${var.gcp_region}-docker.pkg.dev"
   artifact_registry_root       = "${local.artifact_registry_location}/${var.gcp_project}"
   artifact_registry_repository = "${local.system_env_base}repository"
   buz_source_image             = "ghcr.io/silverton-io/buz:${var.buz_version}"
   buz_image                    = "${local.artifact_registry_root}/${local.artifact_registry_repository}/buz:${var.buz_version}"
-  # Config
-  config = "${local.system_env_base}config"
-  # Schema Bucket
-  schema_bucket = "${local.system_env_base}${var.schema_bucket_name}"
-  # Pub/Sub
-  invalid_topic = "${local.system_env_base}${var.invalid_topic_name}"
-  valid_topic   = "${local.system_env_base}${var.valid_topic_name}"
+  config                       = "${local.system_env_base}config"
+  schema_bucket                = "${local.system_env_base}${var.schema_bucket_name}"
+  invalid_topic                = "${local.system_env_base}${var.invalid_topic_name}"
+  valid_topic                  = "${local.system_env_base}${var.valid_topic_name}"
 }
