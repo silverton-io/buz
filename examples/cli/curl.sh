ac649b2c3909700ea34acd6fefb465408ae7fbbf
jakthom <aerialfly@gmail.com>
2022-09-22 22:20:26 -0400

    Put simple config back for bootstrapping purposes, add more curl examples

--- examples/cli/curl.sh
+++ examples/cli/curl.sh
@@ -1,5 +1,5 @@
 ###########################################################################
-# NOTE! These should be turned into tests
+# These should be turned into tests
 ###########################################################################
 
 
@@ -16,11 +16,26 @@ curl -X POST localhost:8080/cloudevents -H 'Content-Type:application/cloudevents
 curl -X POST localhost:8080/cloudevents -H 'Content-Type:application/cloudevents+json' -d '{"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "activity": "didSomething"}}'
 # Invalid event -> valid schema, invalid payload (extra props)
 curl -X POST localhost:8080/cloudevents -H 'Content-Type:application/cloudevents+json' -d '{"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "action": "didSomething", "somethingElse": "bad"}}'
-
 # Valid event batch -> valid schemas, valid payloads
-
+curl -X POST localhost:8080/cloudevents -H 'Content-Type:application/cloudevents+json' -d '[{"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "action": "didSomething"}}, {"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "action": "didSomething"}}]'
 # Mixed event batch
+curl -X POST localhost:8080/cloudevents -H 'Content-Type:application/cloudevents+json' -d '[{"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "action": "didSomething", "somethingElse": "bad"}}, {"dataschema":"io.silverton/buz/example/gettingStarted/v1.0.json", "data": {"userId": 10, "name": "jakthom", "action": "didSomething"}}]'
 
 ###########################################################################
-# 
+# Pixel
 ###########################################################################
+# Valid arbitrary event -> arbitrary schema, valid payload
+curl -X GET "localhost:8080/pixel?id=10"
+# Valid arbitrary event -> explicit schema, valid payload
+curl -X GET "localhost:8080/pixel/io.silverton/buz/example/generic/sample/v1.0?id=10"
+# Invalid arbitrary event -> explicit schema, invalid payload (want int, got string)
+curl -X GET "localhost:8080/pixel/io.silverton/buz/example/gettingStarted/v1.0?userId=10&name=george&action=clicked"
+
+###########################################################################
+# Webhook
+###########################################################################
+# Valid arbitrary event -> arbitrary schema, valid payload
+
+# Valid arbitrary event -> explicit schema, valid payload
+
+# Invalid arbitrary event -> explicit schema, invalid payload
