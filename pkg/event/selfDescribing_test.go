955c40c74ba7a6859a6b096b1556ed4ee896675e
Greg Clunies <greg.clunies@gmail.com>
2022-09-28 03:37:19 -0700

    Update license to Apache 2.0 (#447)
    * update license to apache
    
    * add missing license header

--- pkg/event/selfDescribing_test.go
+++ pkg/event/selfDescribing_test.go
@@ -1,5 +1,5 @@
 // Copyright (c) 2022 Silverton Data, Inc.
-// You may use, distribute, and modify this code under the terms of the AGPLv3 license, a copy of
+// You may use, distribute, and modify this code under the terms of the Apache-2.0 license, a copy of
 // which may be found at https://github.com/silverton-io/buz/blob/main/LICENSE
 
 package event
