f33cae17b8575fea1c83089555b1a0b76f62df85
jakthom <aerialfly@gmail.com>
2022-12-04 22:30:19 -0500

    fix iam

--- deploy/terraform/aws/lambda/data.tf
+++ deploy/terraform/aws/lambda/data.tf
@@ -76,7 +76,7 @@ data "aws_iam_policy_document" "lambda_role_policy" {
     effect = "Allow"
     actions = ["logs:CreateLogStream", "logs:PutLogEvents"]
     resources = [
-      "arn:aws:logs:${data.aws_region.current.name}:log-group:/aws/lambda/${local.service_name}:*"
+      "arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:/aws/lambda/${local.service_name}:*"
     ]
   }
 
