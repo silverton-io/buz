a9debe4c40660699f995890c8537b414885f2c6b
jakthom <aerialfly@gmail.com>
2023-04-17 23:18:47 -0400

    test out buffering within the goroutine

--- pkg/backend/backendutils/sink.go
+++ pkg/backend/backendutils/sink.go
@@ -11,9 +11,10 @@ import (
 	"github.com/rs/zerolog/log"
 	"github.com/silverton-io/buz/pkg/config"
 	"github.com/silverton-io/buz/pkg/envelope"
+	"github.com/silverton-io/buz/pkg/util"
 )
 
-var DEFAULT_SINK_TIMEOUT_SECONDS int = 15
+const MAX_BUFFER_SIZE int = 1
 
 type SinkMetadata struct {
 	Id               uuid.UUID `json:"id"`
@@ -58,29 +59,37 @@ func publish(ctx context.Context, sink Sink, envelopes []envelope.Envelope, outp
 // for dequeuing envelopes.
 func StartSinkWorker(input <-chan []envelope.Envelope, shutdown <-chan int, sink Sink) error {
 	go func(input <-chan []envelope.Envelope, shutdown <-chan int, sink Sink) {
+		var buffer []envelope.Envelope
 		for {
 			select {
 			case envelopes := <-input:
 				// Just handle valid/invalid for now. This will be where events will be further sharded going forward.
-				var invalidEnvelopes []envelope.Envelope
-				var validEnvelopes []envelope.Envelope
-				for _, envelope := range envelopes {
-					if envelope.IsValid {
-						validEnvelopes = append(validEnvelopes, envelope)
-					} else {
-						invalidEnvelopes = append(invalidEnvelopes, envelope)
+				for _, e := range envelopes {
+					buffer = append(buffer, e)
+					if len(buffer) >= MAX_BUFFER_SIZE {
+						var invalidEnvelopes []envelope.Envelope
+						var validEnvelopes []envelope.Envelope
+						for _, envelope := range buffer {
+							if envelope.IsValid {
+								validEnvelopes = append(validEnvelopes, envelope)
+							} else {
+								invalidEnvelopes = append(invalidEnvelopes, envelope)
+							}
+						}
+						ctx := context.Background()
+						// Send good events along
+						err := publish(ctx, sink, validEnvelopes, sink.Metadata().DefaultOutput)
+						if err != nil {
+							log.Error().Err(err).Interface("metadata", sink.Metadata()).Msg("could not publish envelopes to sink")
+						}
+						// Send bad events to deadletter
+						err = publish(ctx, sink, invalidEnvelopes, sink.Metadata().DeadletterOutput)
+						if err != nil {
+							log.Error().Err(err).Interface("metadata", sink.Metadata()).Msg("could not publish envelopes to sink")
+						}
+						util.Pprint("emptying buffer")
+						buffer = make([]envelope.Envelope, 0)
 					}
-				}
-				ctx := context.Background()
-				// Send good events along
-				err := publish(ctx, sink, validEnvelopes, sink.Metadata().DefaultOutput)
-				if err != nil {
-					log.Error().Err(err).Interface("metadata", sink.Metadata()).Msg("could not publish envelopes to sink")
-				}
-				// Send bad events to deadletter
-				err = publish(ctx, sink, invalidEnvelopes, sink.Metadata().DeadletterOutput)
-				if err != nil {
-					log.Error().Err(err).Interface("metadata", sink.Metadata()).Msg("could not publish envelopes to sink")
 				}
 			case <-shutdown:
 				return
