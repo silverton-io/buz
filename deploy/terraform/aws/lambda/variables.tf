be0a44c4347f120271585f7dc94def3b7efd81ac
Jake Thomas <aerialfly@gmail.com>
2023-04-26 23:03:09 -0400

    Switch to goreleaser (#528)
    * switch to goreleaser
    
    * use latest
    
    * try this
    
    * kick patch ahead
    
    * unnecessary ignores

--- deploy/terraform/aws/lambda/variables.tf
+++ deploy/terraform/aws/lambda/variables.tf
@@ -29,7 +29,7 @@ variable "buz_domain" {
 variable "buz_version" {
   description = "The version of Buz to run."
   type        = string
-  default     = "v0.15.7"
+  default     = "v0.15.8"
 }
 
 variable "buz_lambda_memory_limit" {
--- deploy/terraform/gcp/cloud_run/variables.tf
+++ deploy/terraform/gcp/cloud_run/variables.tf
@@ -28,7 +28,7 @@ variable "buz_domain" {
 variable "buz_version" {
   description = "The version of Buz to run."
   type        = string
-  default     = "v0.15.7"
+  default     = "v0.15.8"
 }
 
 variable "buz_service_timeout_seconds" {
