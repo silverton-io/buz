fb6b1d799f98d31cd8569f67932981a3962daf9f
jakthom <aerialfly@gmail.com>
2023-04-28 16:21:46 -0400

    bump

--- deploy/terraform/aws/lambda/variables.tf
+++ deploy/terraform/aws/lambda/variables.tf
@@ -29,7 +29,7 @@ variable "buz_domain" {
 variable "buz_version" {
   description = "The version of Buz to run."
   type        = string
-  default     = "v0.15.12"
+  default     = "v0.15.13"
 }
 
 variable "buz_lambda_memory_limit" {
