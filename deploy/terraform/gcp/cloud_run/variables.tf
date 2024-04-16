b09b763eadbdc45bb1e189954c8ba0d5e523193a
ricky-galvao <ricky8472@gmail.com>
2024-04-16 17:55:31 -0400

    Relax Content-Type expectation and bump version to 0.18.3

--- deploy/terraform/gcp/cloud_run/variables.tf
+++ deploy/terraform/gcp/cloud_run/variables.tf
@@ -28,7 +28,7 @@ variable "buz_domain" {
 variable "buz_version" {
   description = "The version of Buz to run."
   type        = string
-  default     = "v0.18.2"
+  default     = "v0.18.3"
 }
 
 variable "buz_service_timeout_seconds" {
