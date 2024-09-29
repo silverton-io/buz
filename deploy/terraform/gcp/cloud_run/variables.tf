93c65ffd006dafbc85e3d9205b37eba3ad669ef8
jakthom <aerialfly@gmail.com>
2024-09-28 23:01:32 -0400

    Bumps version to 0.19.0 following community changes

--- deploy/terraform/gcp/cloud_run/variables.tf
+++ deploy/terraform/gcp/cloud_run/variables.tf
@@ -28,7 +28,7 @@ variable "buz_domain" {
 variable "buz_version" {
   description = "The version of Buz to run."
   type        = string
-  default     = "v0.18.5"
+  default     = "v0.19.0"
 }
 
 variable "buz_service_timeout_seconds" {
